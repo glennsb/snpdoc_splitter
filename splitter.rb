36fa88fa2b8ef0bc9c89362a6910858dbee6f935
Stuart B. Glenn <glennsb@lupus.omrf.org>
2010-03-26 07:53:21 -0500

    Starting of class to track the snps and investigators

---
+++ splitter.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+#
+# Just a mapper between Snp & Investigators
+#
+class InvestigatorSnpMap
+  
+  def initialize
+    @investigators_snps = {}
+    @snps_investigators = {}
+  end
+  
+  def add(inv,snp)
+    add_investigator_to_snp(inv,snp)
+    add_snp_to_investigator(snp,inv)
+  end
+  
+  :private
+  def add_investigator_to_snp(inv, snp)
+    @snps_investigators[snp] || = []
+    @snps_investigators[snp] << inv
+  end
+  
+  def add_snp_to_investigator(snp,inv)
+    @investigators_snps[inv] ||= []
+    @investigators_snps[inv] << snp
+  end
+end
